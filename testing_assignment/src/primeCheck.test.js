8764251ae9689874a2a243bf18cb1411b2bc57bd
Philip Dushie <philip.dushie@gmail.com>
2025-02-13 13:20:57 -0400

    added test case for primeCheck

---
+++ testing_assignment/src/primeCheck.test.js
@@ -0,0 +1,11 @@
+const primeCheck = require('./primeCheck');
+
+test('tests if 356 is prime', ()=>{
+  expect(primeCheck(356)).toBe(false);
+});
+test('tests if 500299 is prime', ()=>{
+  expect(primeCheck(500299)).toBe(true);
+});
+test('tests if 500315 is prime', ()=>{
+  expect(primeCheck(500315)).toBe(false);
+});
